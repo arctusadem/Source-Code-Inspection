bb79d7f67b0bf0b5cb2c8e7e92d657b31450b911
arctusadem <bruno.cesar.salgado@terra.com.br>
2016-09-05 18:21:36 -0300

    Código atualizado

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -2,51 +2,60 @@ package br.calebe.ticketmachine.core;
 
 import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
 import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
-import java.util.Iterator;
+import java.util.ArrayList;
 
 /**
  *
  * @author Calebe de Paula Bianchini
  */
+
 public class TicketMachine {
 
-    protected int valor;
-    protected int saldo;
-    protected int[] papelMoeda = {2, 5, 10, 20, 50, 100};
+    protected double valorTicket;
+    protected double saldo;
+    protected ArrayList<PapelMoeda> notas_e_moedas;
 
-    public TicketMachine(int valor) {
-        this.valor = valor;
-        this.saldo = 0;
+    public TicketMachine(double valor_do_ticket) {
+        this.valorTicket    = valor_do_ticket;
+        this.saldo          = 0;
+        this.notas_e_moedas = new ArrayList<>();
+        this.notas_e_moedas.add(new PapelMoeda(100.00, 0));
+        this.notas_e_moedas.add(new PapelMoeda( 50.00, 0));
+        this.notas_e_moedas.add(new PapelMoeda( 20.00, 0));
+        this.notas_e_moedas.add(new PapelMoeda( 10.00, 0));
+        this.notas_e_moedas.add(new PapelMoeda(  5.00, 0));
+        this.notas_e_moedas.add(new PapelMoeda(  2.00, 0));
+        this.notas_e_moedas.add(new PapelMoeda(  1.00, 0));
+        this.notas_e_moedas.add(new PapelMoeda(  0.50, 0));
+        this.notas_e_moedas.add(new PapelMoeda(  0.25, 0));
+        this.notas_e_moedas.add(new PapelMoeda(  0.10, 0));
+        this.notas_e_moedas.add(new PapelMoeda(  0.05, 0));
     }
 
     public void inserir(int quantia) throws PapelMoedaInvalidaException {
-        boolean achou = false;
-        for (int i = 0; i < papelMoeda.length && !achou; i++) {
-            if (papelMoeda[1] == quantia) {
-                achou = true;
+        System.out.println("Aguarda alguns instantes...");
+        for (PapelMoeda dinheiro : this.notas_e_moedas) {
+            if (dinheiro.getQuantidade() == quantia) {
+                dinheiro.addQuantidade();
+                this.saldo += quantia;
+                return;
             }
         }
-        if (!achou) {
-            throw new PapelMoedaInvalidaException();
-        }
-        this.saldo += quantia;
+        throw new PapelMoedaInvalidaException();
     }
 
-    public int getSaldo() {
+    public double getSaldo() {
         return saldo;
     }
 
-    public Iterator<Integer> getTroco() {
-        return null;
-    }
-
     public String imprimir() throws SaldoInsuficienteException {
-        if (saldo < valor) {
+        if (saldo < this.valorTicket) {
             throw new SaldoInsuficienteException();
         }
         String result = "*****************\n";
         result += "*** R$ " + saldo + ",00 ****\n";
         result += "*****************\n";
+        System.out.println(result);
         return result;
     }
 }
